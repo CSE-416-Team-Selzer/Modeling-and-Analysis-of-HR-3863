8b8aa3512cbdd0cd9566ab0f4c6b87d2a490e8eb
David Silverman <dsilverman47@gmail.com>
2022-09-19 15:02:24 -0400

    Slightly cleaned up StatePage.js

--- frontend/proj-react/src/StatePage.js
+++ frontend/proj-react/src/StatePage.js
@@ -18,83 +18,10 @@ function StatePage() {
     const [smdCompCarousel, setSMDCompCarousel] = useState(0);
     const [mmdCompCarousel, setMMDCompCarousel] = useState(0);
 
-    const map = (smdOpen) ? (<Col id={"smd-" + stateName}>
-                            <img src={"images/" + stateName + "placeholder.jpg"} alt={"SMD Map of " + styledStateName} class="img-fluid w-50"></img>
-                            <p class="lead">SMD Map of {styledStateName}</p>
-                            </Col>)
-                            : (<Col id={"mmd-" + stateName}>
-                                <img src={"images/" + stateName + "placeholder2.png"} alt={"MMD Map of " + styledStateName} class="img-fluid w-50"></img>
-                                <p class="lead">MMD Map of {styledStateName}</p>
-                            </Col>);
-
-    const stats = (statsCarousel === 0) ? (<div>
-        <img src="images/example-political.png" class="w-50" alt=""></img>
-        <div class="d-none d-md-block">
-            <h5>Political Demographics</h5>
-            <p>Demographics of political parties active in the region.</p>
-            <br></br>
-        </div>
-    </div>) : ((statsCarousel === 1) ? (<div>
-        <img src="images/example-ethnic.png" class="w-50" alt=""></img>
-        <div class="d-none d-md-block">
-            <h5>Ethnic Demographics</h5>
-            <p>Demographics of the ethnic composition of the state.</p>
-            <br></br>
-        </div>
-    </div>) : (<div>
-        <img src="images/example-other.png" class="w-50" alt=""></img>
-        <div class="d-none d-md-block">
-            <h5>Other Demographics</h5>
-            <p>Other interesting demographics; Here pictured are some fake numbers for 'Religiousness.'</p>
-            <br></br>
-        </div>
-    </div>));
-
-    const smdComp = (smdCompCarousel === 0) ? (<div>
-        <img src="images/example-whisker.jpg" class="w-50" alt=""></img>
-        <div class="d-none d-md-block">
-            <h5>Election Wins for a Party</h5>
-            <p>Box and Whisker of election wins compared to supporters in that region.</p>
-            <br></br>
-        </div>
-    </div>) : ((smdCompCarousel === 1) ? (<div>
-        <img src="images/example-whisker.jpg" class="w-50" alt=""></img>
-        <div class="d-none d-md-block">
-            <h5>Ethnic Wins</h5>
-            <p>Demographics of the ethnic composition of the state as compared to the winners of that demographic.</p>
-            <br></br>
-        </div>
-    </div>) : (<div>
-        <img src="images/example-whisker.jpg" class="w-50" alt=""></img>
-        <div class="d-none d-md-block">
-            <h5>Other Wins</h5>
-            <p>Other interesting demographics; Here pictured are some fake numbers for 'Religiousness.'</p>
-            <br></br>
-        </div>
-    </div>));
-
-    const mmdComp = (mmdCompCarousel === 0) ? (<div>
-        <img src="images/example-whisker.jpg" class="w-50" alt=""></img>
-        <div class="d-none d-md-block">
-            <h5>Election Wins for a Party</h5>
-            <p>Box and Whisker of election wins compared to supporters in that region.</p>
-            <br></br>
-        </div>
-    </div>) : ((mmdCompCarousel === 1) ? (<div>
-        <img src="images/example-whisker.jpg" class="w-50" alt=""></img>
-        <div class="d-none d-md-block">
-            <h5>Ethnic Wins</h5>
-            <p>Demographics of the ethnic composition of the state as compared to the winners of that demographic.</p>
-            <br></br>
-        </div>
-    </div>) : (<div>
-        <img src="images/example-whisker.jpg" class="w-50" alt=""></img>
-        <div class="d-none d-md-block">
-            <h5>Other Wins</h5>
-            <p>Other interesting demographics; Here pictured are some fake numbers for 'Religiousness.'</p>
-            <br></br>
-        </div>
-    </div>));
+    const map = <Col id={(smdOpen ? "s" : "m") + "md-" + stateName}>
+                    <img src={"images/" + stateName + "placeholder" + (smdOpen ? "" : "2") + ".jpg"} alt={(smdOpen ? "S" : "M") + "MD Map of " + styledStateName} class="img-fluid w-50"></img>
+                    <p class="lead">{(smdOpen ? "S" : "M") + "MD Map of " + styledStateName}</p>
+                </Col>;
 
     return (
         <div>
@@ -134,7 +61,14 @@ function StatePage() {
                 <Row className="text-center">
                     <h2>Interesting Statistics</h2>
                     <Col>
-                        {stats}
+                        <div>
+                            <img src={"images/example-" + (statsCarousel === 0 ? "political" : (statsCarousel === 1 ? "ethnic" : "other")) + ".png"} class="w-50" alt=""></img>
+                            <div class="d-none d-md-block">
+                                <h5>{(statsCarousel === 0 ? "Political" : (statsCarousel === 1 ? "Ethnic" : "Other")) + " Demographics"}</h5>
+                                <p>{statsCarousel === 0 ? "Demographics of political parties active in the region." : (statsCarousel === 1 ? "Demographics of the ethnic composition of the state." : "Other interesting demographics; Here pictured are some fake numbers for 'Religiousness.'")}</p>
+                                <br></br>
+                            </div>
+                        </div>
                         <Button variant="dark" onClick={() => setStatsCarousel((statsCarousel+1)%3)}>Next Statistic</Button>
                     </Col>
                 </Row>
@@ -143,11 +77,25 @@ function StatePage() {
                 <Row className="text-center">
                     <h2>SMD vs MMD Comparison</h2>
                     <Col>
-                        {smdComp}
+                        <div>
+                            <img src="images/example-whisker.jpg" class="w-50" alt=""></img>
+                            <div class="d-none d-md-block">
+                                <h5>{smdCompCarousel === 0 ? "Election Wins for a Party" : (smdCompCarousel === 1 ? "Ethnic Wins" : "Other Wins")}</h5>
+                                <p>{smdCompCarousel === 0 ? "Box and Whisker of election wins compared to supporters in that region." : (smdCompCarousel === 1 ? "Demographics of the ethnic composition of the state as compared to the winners of that demographic." : "Other interesting demographics; Here pictured are some fake numbers for 'Religiousness.'")}</p>
+                                <br></br>
+                            </div>
+                        </div>
                         <Button variant="dark" size="sm" onClick={() => setSMDCompCarousel((smdCompCarousel+1)%3)}>Next Statistic</Button>
                     </Col>
                     <Col>
-                        {mmdComp}
+                        <div>
+                            <img src="images/example-whisker.jpg" class="w-50" alt=""></img>
+                            <div class="d-none d-md-block">
+                                <h5>{mmdCompCarousel === 0 ? "Election Wins for a Party" : (mmdCompCarousel === 1 ? "Ethnic Wins" : "Other Wins")}</h5>
+                                <p>{mmdCompCarousel === 0 ? "Box and Whisker of election wins compared to supporters in that region." : (mmdCompCarousel === 1 ? "Demographics of the ethnic composition of the state as compared to the winners of that demographic." : "Other interesting demographics; Here pictured are some fake numbers for 'Religiousness.'")}</p>
+                                <br></br>
+                            </div>
+                        </div>
                         <Button variant="dark" size="sm" onClick={() => setMMDCompCarousel((mmdCompCarousel+1)%3)}>Next Statistic</Button>
                     </Col>
                 </Row>
