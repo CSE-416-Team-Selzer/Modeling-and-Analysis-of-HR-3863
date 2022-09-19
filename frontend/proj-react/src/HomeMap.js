97103c391b8f73df14ab2c00469cb8dd03040610
AZaft <ziafatanees@gmail.com>
2022-09-19 03:48:50 -0400

    Added leaflet map component with some interactivity

---
+++ frontend/proj-react/src/HomeMap.js
@@ -0,0 +1,86 @@
+import { React, useState, useMemo }from "react";
+import { MapContainer, TileLayer, useMap, Popup, Marker, GeoJSON } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+const states = require('./geoJSON/us-states.json');
+
+const position = [39, -98];
+
+function SetBoundsStates() {
+    const [bounds, setBounds] = useState()
+    const map = useMap()
+  
+    const innerHandlers = useMemo(
+      () => ({
+        click(e) {
+            console.log(e.target);
+            map.setView([e.latlng.lat, e.latlng.lng], 6);
+        },
+      }),
+      [map],
+    )
+
+    function highlightState(e) {
+        var layer = e.target;
+
+        layer.setStyle({
+            color: 'red',
+        });
+    
+       
+        layer.bringToFront();
+    }
+
+    function resetHighlight(e) {
+        var layer = e.target;
+
+        layer.setStyle({
+            color: 'blue',
+        });
+    
+       
+        layer.bringToFront();
+    }
+
+
+    function zoomToState(e) {
+        map.fitBounds(e.target.getBounds());
+    }
+
+    function onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: highlightState,
+            mouseout: resetHighlight,
+            click: zoomToState
+        });
+    }
+  
+    return (
+      <>
+        <GeoJSON
+        data={states}
+        onEachFeature= {onEachFeature}
+        pathOptions={ {color: 'blue'}}
+        />
+      </>
+    )
+  }
+
+
+function HomeMap() {
+    return(
+        <MapContainer 
+            style={{ height: 500, width: 1000 }} 
+            center={position} 
+            zoom={2.5} 
+            scrollWheelZoom={true}
+        >
+            <SetBoundsStates />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    );
+}
+
+export default HomeMap;
--- frontend/proj-react/src/HomePage.js
+++ frontend/proj-react/src/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import StatesNavbar from './StatesNavbar.js';
+import HomeMap from './HomeMap.js';
 
 export default class HomePage extends React.Component {
     render() {
@@ -8,6 +9,7 @@ export default class HomePage extends React.Component {
                 <StatesNavbar />
                 <p>Welcome to the homepage. We're not really sure what this will look like in the end product, but on the navbar you'll see several options for the states.</p>
                 <p>Click through those to get to the state pages.</p>
+                <HomeMap />
             </div>
         )
     }
