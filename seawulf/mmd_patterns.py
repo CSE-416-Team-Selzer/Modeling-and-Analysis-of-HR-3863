5c765a0284caf3481f0fbfe09e9f6f5760c1fc2b
David Silverman <dsilverman47@gmail.com>
2022-11-22 18:39:09 -0500

    seawulf-4 exports to files now

--- seawulf/mmd_patterns.py
+++ seawulf/mmd_patterns.py
@@ -1,3 +1,5 @@
+import json
+
 def generate_patterns(number):
     patterns = []
     if number == 3:
@@ -35,7 +37,15 @@ def generate_patterns(number):
                 patterns_no_dupes.append(pattern)
         return patterns_no_dupes
 
-print("Pick a number:")
-rep_count = int(input())
-mmd_patterns = generate_patterns(rep_count)
-print(mmd_patterns)
+
+mmd_patterns_Arizona = generate_patterns(9)
+with open('mmd_patterns_Arizona.json', 'w') as f:
+    json.dump(mmd_patterns_Arizona, f)
+
+mmd_patterns_Texas = generate_patterns(38)
+with open('mmd_patterns_Texas.json', 'w') as f:
+    json.dump(mmd_patterns_Texas, f)
+
+mmd_patterns_Utah = generate_patterns(4)
+with open('mmd_patterns_Utah.json', 'w') as f:
+    json.dump(mmd_patterns_Utah, f)
