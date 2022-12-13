dee1bbbeab746c32d59988936a84ce33e4711f3a
Samuel Steiger Desktop <samcsteiger@gmail.com>
2022-12-13 00:01:11 -0500

    Added Sources

--- proj-react/src/sources_m.js
+++ proj-react/src/sources_m.js
@@ -21,12 +21,18 @@ class SourcesPage extends React.Component {
                             <Col>
                                 <h3>Sources Used:</h3>
                                 <p>
-                                    Source 1 <br/>
-                                    Source 2 <br/>
-                                    Source 3 <br/>
-                                    Source 4 <br/>
-                                    Source 5 <br/>
-                                    etc
+                                    House of Representatives Voting Data:<br/>
+                                    <a href="https://www.census.gov/geographies/mapping-files/time-series/geo/tiger-line-file.2020.html#list-tab-BG8ZITUQ783GX73G14">MIT Election Data and Science Lab, 2022, "U.S. House of Representatives Precinct-Level Returns 2020"</a><br/>
+                                    Boundary Data: <br/>
+                                    <a href="https://www.census.gov/geographies/mapping-files/time-series/geo/tiger-line-file.2020.html#list-tab-BG8ZITUQ783GX73G14">US Census Bureau Tiger/Line Shapefiles</a><br/>
+                                    Racial Demographics Data: <br/>
+                                    <a href="https://data.census.gov/table?q=race&y=2020">U.S. Census Bureau 2020. P2: HISPANIC OR LATINO, AND NOT HISPANIC OR LATINO BY RACE.</a><br/>
+                                    Arizona Current Districting: <br/>
+                                    <a href="https://redistricting-irc-az.hub.arcgis.com/pages/official-maps">Arizona Independent Redistricting Commission, Approved Official Congressional Map</a><br/>
+                                    Texas Current Districting: <br/>
+                                    <a href="https://redistricting.capitol.texas.gov/Current-districts">Texas Legislative Council</a><br/>
+                                    Utah Current Districting:<br/>
+                                    <a href="https://citygate.utleg.gov/legdistricting/utah/comment_links">Utah Legislative Redistricting Committee</a><br/>
                                 </p>
                             </Col>
                         </Row>
